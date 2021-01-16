2955a1d23e5679ad4d863aa50de3fecdad337628
Raul <sperniacraul@yahoo.com>
2021-01-16 22:22:29 +0200

    First site

---
+++ restaurant/app.js
@@ -0,0 +1,8 @@
+const drop_down = document.getElementById('nav-links')
+const burger = document.getElementById('burger-menu')
+
+burger.addEventListener('click', ()=>{
+    drop_down.classList.toggle("toggle");
+    
+    
+})
